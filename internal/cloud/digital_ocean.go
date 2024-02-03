6dd747ee1d37052d537b728563191adc951f80dc
Peter Halasz <peter.halasz@live.com>
2024-02-03 20:25:59 +0100

    delete works

--- internal/cloud/digital_ocean.go
+++ internal/cloud/digital_ocean.go
@@ -3,8 +3,11 @@ package cloud
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math"
 	"os"
 	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 
@@ -105,23 +108,56 @@ func (p *DigitalOceanProvider) StopWorkstation(params *WorkstationStopParams) er
 	return errors.New("Stopping a workstation is not implemented yet")
 }
 
-// TODO: Detach volume, wait for it, then delete
+// TODO: Wait until the workstation (and the volume) is 5 minutes old at last before deleting
 func (p *DigitalOceanProvider) DeleteWorkstation(params *WorkstationDeleteParams) error {
 	status, _ := p.GetStatus()
+	if !status.IsActive {
+		fmt.Println("Nothing to delete, there is no active workstation")
+		return nil
+	}
 
+	fmt.Println(status.CreatedAt)
+	now := time.Now()
+	created, _ := time.Parse(time.RFC3339, status.CreatedAt)
+
+	workstation_age := int(math.Floor(now.Sub(created).Minutes()))
+
+	if workstation_age < 5 {
+		fmt.Printf("Workstation can't be deleted until at least 5 minutes old. Current age: %d minutes\n", workstation_age)
+		return nil
+	}
+
+	log.Debugf("Detaching workstation %d from volume %s", status.ID, status.Volume)
 	_, _, err := p.client.StorageActions.DetachByDropletID(context.TODO(), status.Volume, status.ID)
 
 	if err != nil {
+		log.Debugf("Error %s", err.Error())
 		return err
 	}
+	log.Debugf("Workstation %d detached from volume %s", status.ID, status.Volume)
 
-	_, err = p.client.Droplets.DeleteByTag(context.TODO(), "workstation")
+	log.Debugf("Sleeping for 5 seconds")
+	time.Sleep(5 * time.Second)
+
+	log.Debugf("Deleting workstation %d", status.ID)
+	_, err = p.client.Droplets.Delete(context.TODO(), status.ID)
 
 	if err != nil {
+		log.Debugf("Error %s", err.Error())
 		return err
 	}
+	log.Debugf("Workstation %d deleted", status.ID)
 
+	log.Debugf("Sleeping for 5 seconds")
+	time.Sleep(5 * time.Second)
+
+	log.Debugf("Deleting volume %s", status.Volume)
 	_, err = p.client.Storage.DeleteVolume(context.TODO(), status.Volume)
+	if err != nil {
+		log.Debugf("Error %s", err.Error())
+		return err
+	}
+	log.Debugf("Volume %s deleted", status.ID)
 
 	return err
 }
