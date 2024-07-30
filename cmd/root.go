0c7a43e4a57b96ba9174dc1181744fd6acafe9d4
Peter Halasz <peter.halasz@live.com>
2024-07-30 19:59:27 +0200

    move config to util

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/peterhalasz/envoi/internal/util"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -18,42 +17,8 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-func initDefaultConfigValues() {
-	viper.SetDefault("digitalocean.tag", "envoi")
-	viper.SetDefault("digitalocean.region", "fra1")
-
-	viper.SetDefault("digitalocean.volume.enabled", true)
-	viper.SetDefault("digitalocean.volume.name", "envoi")
-	viper.SetDefault("digitalocean.volume.file_system_type", "ext4")
-	viper.SetDefault("digitalocean.volume.file_system_label", "envoi")
-	viper.SetDefault("digitalocean.volume.size_gb", 5)
-
-	viper.SetDefault("digitalocean.droplet.name", "envoi")
-	viper.SetDefault("digitalocean.droplet.size", "s-1vcpu-512mb-10gb")
-	viper.SetDefault("digitalocean.droplet.image", "ubuntu-23-10-x64")
-}
-
-func initConfig() {
-	initDefaultConfigValues()
-
-	log.Debug("Reading config")
-	viper.SetConfigName(".envoi.conf")
-	viper.SetConfigType("yaml")
-	//viper.AddConfigPath("/etc/appname/")
-	//viper.AddConfigPath("$HOME/.appname")
-	viper.AddConfigPath(".")
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			panic(fmt.Errorf("config file not found"))
-		} else {
-			panic(fmt.Errorf("fatal error config file: %w", err))
-		}
-	}
-}
-
 func Execute() {
-	initConfig()
+	util.InitConfig()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
---
+++ internal/util/config.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func initDefaultConfigValues() {
+	viper.SetDefault("digitalocean.tag", "envoi")
+	viper.SetDefault("digitalocean.region", "fra1")
+
+	viper.SetDefault("digitalocean.volume.enabled", true)
+	viper.SetDefault("digitalocean.volume.name", "envoi")
+	viper.SetDefault("digitalocean.volume.file_system_type", "ext4")
+	viper.SetDefault("digitalocean.volume.file_system_label", "envoi")
+	viper.SetDefault("digitalocean.volume.size_gb", 5)
+
+	viper.SetDefault("digitalocean.droplet.name", "envoi")
+	viper.SetDefault("digitalocean.droplet.size", "s-1vcpu-512mb-10gb")
+	viper.SetDefault("digitalocean.droplet.image", "ubuntu-23-10-x64")
+}
+
+func InitConfig() {
+	initDefaultConfigValues()
+
+	log.Debug("Reading config")
+	viper.SetConfigName(".envoi.conf")
+	viper.SetConfigType("yaml")
+	//viper.AddConfigPath("/etc/appname/")
+	//viper.AddConfigPath("$HOME/.appname")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			panic(fmt.Errorf("config file not found"))
+		} else {
+			panic(fmt.Errorf("fatal error config file: %w", err))
+		}
+	}
+}
